5a5c15e8ba026e10923a646185f49a0c928a62f8
Lukas Hamacek <xhamacek@stuba.sk>
2018-01-20 20:44:59 +0100

    RafactorFix

--- MyTestProject/src/AnotherClass.java
+++ MyTestProject/src/AnotherClass.java
@@ -1,7 +1,15 @@
+//SONAR_0 : Move this file to a named package.
 
-public class AnotherClass {
+//SONAR_1 : Add a private constructor to hide the implicit public one.
+//REFACTOR - Lazy Class
+  public class AnotherClass {
+//SONAR_1_END
 	
+//SONAR_2 : Rename this method name to match the regular expression '^[a-z][a-zA-Z0-9]*$'.
 	public static void Print(){
+//SONAR_2_END
+//SONAR_3 : Replace this use of System.out or System.err by a logger.
 		System.out.println("x");
+//SONAR_3_END
 	}
 }
--- MyTestProject/src/MainClass.java
+++ MyTestProject/src/MainClass.java
@@ -1,9 +1,18 @@
+//SONAR_4 : Move this file to a named package.
 
-public class MainClass {
+//REFACTOR - Lazy Class
+  public class MainClass {
 
-	public static void main(String[] args) {
+	//REFACTOR - Long Method
+  public static void main(String[] args) {
+//SONAR_5 : Remove this unused "x" local variable.
+//SONAR_6 : Remove this useless assignment to local variable "x".
 		int x = 5;
+//SONAR_6_END
+//SONAR_5_END
+//SONAR_7 : Replace this use of System.out or System.err by a logger.
 		System.out.println("Hello from my test app!!");
+//SONAR_7_END
 
 	}
 
