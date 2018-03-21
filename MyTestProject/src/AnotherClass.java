f9d33aa296a24cc88bd94cf037c0b621483a1158
Lukas Hamacek <xhamacek@stuba.sk>
2018-03-21 19:36:50 +0100

    RafactorFix

--- MyTestProject/src/AnotherClass.java
+++ MyTestProject/src/AnotherClass.java
@@ -1,5 +1,11 @@
+//SMELL: #SmellType(Move this file to a named package. [SONAR])
+//SMELL: #SmellType(Add a private constructor to hide the implicit public one. [SONAR])
 
-public class AnotherClass {
+//SMELL: #SmellType(Rename this method name to match the regular expression '^[a-z][a-zA-Z0-9]*$'. [SONAR])
+//SMELL: #SmellType(Replace this use of System.out or System.err by a logger. [SONAR])
+//REFACTOR - Lazy Class
+  //SMELL: #SmellType(Lazy Class)
+ public class AnotherClass {
 	
 	public static void Print(){
 		System.out.println("x");
--- MyTestProject/src/MainClass.java
+++ MyTestProject/src/MainClass.java
@@ -1,6 +1,12 @@
+//SMELL: #SmellType(Move this file to a named package. [SONAR])
 
-public class MainClass {
+//REFACTOR - Lazy Class
+  //SMELL: #SmellType(Lazy Class)
+ public class MainClass {
 
+//SMELL: #SmellType(Remove this useless assignment to local variable "x". [SONAR])
+//SMELL: #SmellType(Replace this use of System.out or System.err by a logger. [SONAR])
+//SMELL: #SmellType(Remove this unused "x" local variable. [SONAR])
 	public static void main(String[] args) {
 		int x = 5;
 		System.out.println("Hello from my test app!!");
